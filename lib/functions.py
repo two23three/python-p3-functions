fdf7050ec77e24a19279032fcbf49a42ef9eb8ae
Tulley Mwenda <Tulleyteyez@gmail.com>
2024-05-21 08:52:00 +0300

    done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
 
 def greet(name):
+    print(f"Hello, {name}!")
     pass
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
     pass
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+
 
 def halve(number):
-    pass
+    return number / 2
+def my_function(param):
+    print("Running my_function")
+    return param + 1
